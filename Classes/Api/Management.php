c6d909908ed9310e7f057ad4c91d607e1d0f1043
Robert Lemke <robert@flownative.com>
2019-08-27 19:02:14 +0200

    Remove Flow Auth code; add Manager API

---
+++ Classes/Api/Management.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Flownative\Auth0\Api;
+
+/*
+ * This file is part of the Flownative.Auth0 package.
+ *
+ * (c) Robert Lemke, Flownative GmbH - www.flownative.com
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\Flow\Annotations as Flow;
+
+/**
+ * Wrapper for the Auth0 Management class.
+ *
+ * The constructor options are configured via Flow Objects configuration
+ *
+ * @Flow\Scope("singleton")
+ */
+class Management extends \Auth0\SDK\API\Management
+{
+}
